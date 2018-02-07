ef30c14da4b95beda3c1506e8aeade677b31781a
康岩 <kyyblabla@163.com>
2018-02-07 18:56:23 +0800

    init

---
+++ wrms-bootstrap/src/main/java/wrms/WrmsApplication.java
@@ -0,0 +1,12 @@
+package wrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrmsApplication.class, args);
+    }
+}
